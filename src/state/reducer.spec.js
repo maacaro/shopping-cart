f5c11e78b3bbedd40d19274d21f82e7bb4b48855
Manuel Castro <maacaro@gmail.com>
2020-06-30 11:19:06 -0400

    Refactor products reducer by adding test

--- src/state/reducer.spec.js
+++ src/state/reducer.spec.js
@@ -1,4 +1,4 @@
-import { cart as cartReducer } from "./reducer"
+import { cart as cartReducer, products as productsReducer } from "./reducer"
 
 describe("cart Reducer", () => {
   it("return the initial state", () => {
@@ -14,6 +14,32 @@ describe("cart Reducer", () => {
     const state = cartReducer([], {
       type: "ADD_ITEM",
       payload: { id: 0, desc: "foo" },
+    })
+
+    expect(state).toEqual(expectedState)
+  })
+})
+
+describe("product reducer", () => {
+  it("return the initial state", () => {
+    const initialState = []
+
+    const state = productsReducer(null, {})
+
+    expect(state).toEqual(initialState)
+  })
+  it("add a list of products to the state", () => {
+    const expectedState = [
+      { id: 0, desc: "foo" },
+      { id: 1, desc: "bar" },
+    ]
+
+    const state = productsReducer([], {
+      type: "LOAD_PRODUCTS",
+      payload: [
+        { id: 0, desc: "foo" },
+        { id: 1, desc: "bar" },
+      ],
     })
 
     expect(state).toEqual(expectedState)
