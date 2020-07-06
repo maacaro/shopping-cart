ef009a65f6362f7bbab24de123464c9f9c5f6379
Manuel Castro <maacaro@gmail.com>
2020-07-06 08:16:03 -0400

    Bump version

--- src/state/createStore.js
+++ src/state/createStore.js
@@ -1,9 +1,12 @@
 import { createStore as reduxCreateStore, combineReducers } from "redux"
 import { cart, products } from "./reducer"
+const windowExist = typeof window === "object"
 
 const createStore = () =>
   reduxCreateStore(
     combineReducers({ cart, products }),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    windowExist &&
+      window.__REDUX_DEVTOOLS_EXTENSION__ &&
+      window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 export default createStore
